61179a84a387745e27b048c4c0d9c7fedd3d388b
Laura Messa Travin <laura.travin@gmail.com>
2021-03-02 04:23:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,20 +13,6 @@ class SongsController < ApplicationController
         erb :'songs/new'
    end
 
-
-
-#solution
-  #  post '/songs' do
-  #    @song = Song.create(params[:song])
-  #    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-  #    @song.genre_ids = params[:genres]
-  #    @song.save
-   #
-  #    flash[:message] = "Successfully created song."
-  #    redirect("/songs/#{@song.slug}")
-  #  end
-
-   #
    post '/songs' do
 
           @song = Song.create(params[:song])  #same params[:song][:name]
